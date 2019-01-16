9c4b24db5a36c31f7ec52a7b7be24a077bde5a42
wq-tang <291435820@qq.com>
2019-01-16 17:21:15 +0800

    relu 更新

--- model.py
+++ model.py
@@ -12,6 +12,10 @@ from functools import reduce
 
 # define different layer functions
 # we usually don't do convolution and pooling on batch and channel
+def sign(x):
+    e = 0.1**8
+    return tf.nn.relu(x)/(tf.nn.relu(x)+e)
+
 def maxPoolLayer(x, ksize,strides=[1,1], name='None', padding = "SAME"):
     """max-pooling"""
     return tf.nn.max_pool(x, ksize =[1]+ ksize+[1],
@@ -69,55 +73,6 @@ def myconvLayer(x, ksize, strides,out_channel, name, padding = "SAME"):
         return tf.nn.relu(out, name = scope.name)
         
 
-def complex_fcLayer(x, input_size, output_size, reluFlag, name):
-    """fully-connect"""
-    with tf.variable_scope(name) as scope:
-        wr = tf.get_variable("wr", shape = [input_size, output_size], dtype = tf.float32)
-        wi = tf.get_variable("wi", shape = [input_size, output_size], dtype = tf.float32)
-        br = tf.get_variable("br", [output_size], dtype = tf.float32)
-        bi = tf.get_variable("bi", [output_size], dtype = tf.float32)
-        R = tf.nn.xw_plus_b(x[0], wr, br)- tf.nn.xw_plus_b(x[1], wi, bi)
-        I = tf.nn.xw_plus_b(x[0], wi, bi)+ tf.nn.xw_plus_b(x[1], wr, br)
-        if reluFlag:
-        #     f = tf.complex(R,I)
-        #     f = tf.tanh(f)
-        #     return [tf.real(f),tf.imag(f)]
-            return [tf.nn.relu(R*I),tf.nn.relu(R*I)]
-        else:
-            Z = tf.complex(R,I)
-            Z=1/Z
-            return [tf.real(Z),tf.imag(Z)]
-def complex_convLayer(x, ksize, strides,out_channel, name, padding = "SAME",act_flag=False): 
-    """convolution"""
-    in_channel = int(x[0].get_shape()[-1])
-
-    conv = lambda a, b: tf.nn.conv2d(a, b, strides = [1] +strides +[ 1], padding = padding)
-
-    with tf.variable_scope(name) as scope:
-        wr = tf.get_variable("wr", shape = ksize+[in_channel,out_channel],dtype=tf.float32)
-        wi = tf.get_variable("wi", shape = ksize+[in_channel,out_channel],dtype=tf.float32)
-
-        br = tf.get_variable("br", shape = [out_channel],dtype=tf.float32)
-        bi = tf.get_variable("bi", shape = [out_channel],dtype=tf.float32)
-
-        R = conv(x[0],wr)-conv(x[1],wi) +br
-        I= conv(x[1],wr)+conv(x[0],wi) +bi
-        if act_flag:
-            Z = tf.complex(R,I)
-            Z=1/Z
-            return [tf.real(Z),tf.imag(Z)]
-        else:
-            return [tf.nn.relu(R),I]
-
-        # print mergeFeatureMap.shape
-        # return [tf.nn.relu(R),tf.nn.relu(I)]
-
-def complex_maxPoolLayer(x, ksize,strides=[1,1], name='None', padding = "SAME"):
-    """max-pooling"""
-    return [tf.nn.max_pool(x[0], ksize =[1]+ ksize+[1],
-                          strides = [1] +strides+[1], padding = padding, name = name+'0'),tf.nn.max_pool(x[1], ksize =[1]+ ksize+[1],
-                          strides = [1] +strides+[1], padding = padding, name = name)]
-
 
 
 class alexNet(object):
@@ -163,31 +118,102 @@ class alexNet(object):
             R = tf.reshape(pool4[0],[-1,mul])
             I = tf.reshape(pool4[1],[-1,mul])
             dim = R.get_shape()[1].value
+            self.real_fc(R,I,dim)
+            # self.complex_fc(R,I,dim)
 
-            reshape = tf.sqrt(tf.square(R)+tf.square(I))
-            norm_reshape=tf.layers.batch_normalization(reshape,training=self.training)
-            fc1 = fcLayer(norm_reshape, dim, 512, reluFlag=True, name = "fc4")
-            self.out1 = fc1[0]
-            norm_fc1=tf.layers.batch_normalization(fc1,training=self.training)
-            fc2 = fcLayer(norm_fc1, 512, 128, reluFlag=True,name =  "fc5")
-            self.out2 = fc2[0]
-            norm_fc2=tf.layers.batch_normalization(fc2,training=self.training)
-            self.fc3 = fcLayer(norm_fc2, 128, self.CLASSNUM, reluFlag=True,name =  "fc6")
+    def real_fc(self,R,I,dim):
+        reshape = tf.sqrt(tf.square(R)+tf.square(I))
+        norm_reshape=tf.layers.batch_normalization(reshape,training=self.training)
+        fc1 = fcLayer(norm_reshape, dim, 512, reluFlag=True, name = "fc4")
+        self.out1 = fc1[0]
+        norm_fc1=tf.layers.batch_normalization(fc1,training=self.training)
+        fc2 = fcLayer(norm_fc1, 512, 128, reluFlag=True,name =  "fc5")
+        self.out2 = fc2[0]
+        norm_fc2=tf.layers.batch_normalization(fc2,training=self.training)
+        self.fc3 = fcLayer(norm_fc2, 128, self.CLASSNUM, reluFlag=True,name =  "fc6")
             
-       
-            # R=tf.layers.batch_normalization(R,training=self.training)
-            # I=tf.layers.batch_normalization(I,training=self.training)
-            # fc1 = complex_fcLayer([R,I], dim, 512, reluFlag=True, name = "fc4")
-            # self.out1 = fc1[0]
-            # R=tf.layers.batch_normalization(fc1[0],training=self.training)
-            # I=tf.layers.batch_normalization(fc1[1],training=self.training)
-            # fc2 = complex_fcLayer([R,I], 512, 128, reluFlag=True,name =  "fc5")
-            # self.out2 = fc2[0]
-            # R=tf.layers.batch_normalization(fc2[0],training=self.training)
-            # I=tf.layers.batch_normalization(fc2[1],training=self.training)
-            # fc3 = complex_fcLayer([R,I], 128, self.CLASSNUM, reluFlag=True,name =  "fc6")
-            # self.out3 = fc3[0]
-            # self.fc3 = tf.sqrt(tf.square(fc3[0])+tf.square(fc3[1]))
+    def complex_fc(self,R,I,dim):
+        R=tf.layers.batch_normalization(R,training=self.training)
+        I=tf.layers.batch_normalization(I,training=self.training)
+        fc1 = complex_fcLayer([R,I], dim, 512, reluFlag=True, name = "fc4")
+        self.out1 = fc1[0]
+        R=tf.layers.batch_normalization(fc1[0],training=self.training)
+        I=tf.layers.batch_normalization(fc1[1],training=self.training)
+        fc2 = complex_fcLayer([R,I], 512, 128, reluFlag=True,name =  "fc5")
+        self.out2 = fc2[0]
+        R=tf.layers.batch_normalization(fc2[0],training=self.training)
+        I=tf.layers.batch_normalization(fc2[1],training=self.training)
+        fc3 = complex_fcLayer([R,I], 128, self.CLASSNUM, reluFlag=True,name =  "fc6")
+        self.out3 = fc3[0]
+        self.fc3 = tf.sqrt(tf.square(fc3[0])+tf.square(fc3[1]))
+
+    def Learnable_relu(self,C,name):
+        with tf.variable_scope(name) as scope:
+            alpha = tf.get_variable("alpha",shape = [1],dtype=tf.float32)
+            beita = tf.get_variable("beita",shape = [1],dtype=tf.float32)
+            return [C[0]*sign(tf.atan(C[1]/C[0]-alpha))*sign(alpha+beita-tf.atan(C[1]/C[0]-alpha)),
+            C[1]*sign(tf.atan(C[1]/C[0]-alpha))*sign(alpha+beita-tf.atan(C[1]/C[0]-alpha))]
+
+
+
+
+
+    def complex_fcLayer(self,x, input_size, output_size, reluFlag, name):
+        """fully-connect"""
+        with tf.variable_scope(name) as scope:
+            wr = tf.get_variable("wr", shape = [input_size, output_size], dtype = tf.float32)
+            wi = tf.get_variable("wi", shape = [input_size, output_size], dtype = tf.float32)
+            br = tf.get_variable("br", [output_size], dtype = tf.float32)
+            bi = tf.get_variable("bi", [output_size], dtype = tf.float32)
+            R = tf.nn.xw_plus_b(x[0], wr, br)- tf.nn.xw_plus_b(x[1], wi, bi)
+            I = tf.nn.xw_plus_b(x[0], wi, bi)+ tf.nn.xw_plus_b(x[1], wr, br)
+            if reluFlag:
+            #     f = tf.complex(R,I)
+            #     f = tf.tanh(f)
+            #     return [tf.real(f),tf.imag(f)]
+                return [tf.nn.relu(R*I),tf.nn.relu(R*I)]
+            else:
+                Z = tf.complex(R,I)
+                Z=1/Z
+                return [tf.real(Z),tf.imag(Z)]
+    def complex_convLayer(self,x, ksize, strides,out_channel, name, padding = "SAME",act_flag=False): 
+        """convolution"""
+        in_channel = int(x[0].get_shape()[-1])
+
+        conv = lambda a, b: tf.nn.conv2d(a, b, strides = [1] +strides +[ 1], padding = padding)
+
+        with tf.variable_scope(name) as scope:
+            wr = tf.get_variable("wr", shape = ksize+[in_channel,out_channel],dtype=tf.float32)
+            wi = tf.get_variable("wi", shape = ksize+[in_channel,out_channel],dtype=tf.float32)
+
+            br = tf.get_variable("br", shape = [out_channel],dtype=tf.float32)
+            bi = tf.get_variable("bi", shape = [out_channel],dtype=tf.float32)
+
+            R = conv(x[0],wr)-conv(x[1],wi) +br
+            I= conv(x[1],wr)+conv(x[0],wi) +bi
+            if act_flag:
+                Z = tf.complex(R,I)
+                Z=1/Z
+                return [tf.real(Z),tf.imag(Z)]
+            else:
+                return Learnable_relu([R,I],'relu')
+
+            # print mergeFeatureMap.shape
+            # return [tf.nn.relu(R),tf.nn.relu(I)]
+
+    def complex_maxPoolLayer(self,x, ksize,strides=[1,1], name='None', padding = "SAME"):
+        """max-pooling"""
+        return [tf.nn.max_pool(x[0], ksize =[1]+ ksize+[1],
+                              strides = [1] +strides+[1], padding = padding, name = name+'0'),tf.nn.max_pool(x[1], ksize =[1]+ ksize+[1],
+                              strides = [1] +strides+[1], padding = padding, name = name)]
+
+
+
+
+
+
+
+
 
 
 
