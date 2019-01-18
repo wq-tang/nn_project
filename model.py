98cf11516791cb2969ba7837b5b69302e7b7d4aa
wq-tang <291435820@qq.com>
2019-01-18 10:26:31 +0800

    整改norm位置

--- model.py
+++ model.py
@@ -23,30 +23,7 @@ def maxPoolLayer(x, ksize,strides=[1,1], name='None', padding = "SAME"):
 
 
 
-def fcLayer(x, input_size, output_size, reluFlag, name):
-    """fully-connect"""
-    with tf.variable_scope(name) as scope:
-        w = tf.get_variable("w", shape = [input_size, output_size], dtype = "float")
-        b = tf.get_variable("b", [output_size], dtype = "float")
-        out = tf.nn.xw_plus_b(x, w, b, name = scope.name)
-        if reluFlag:
-            return tf.nn.relu(out)
-        else:
-            return out
 
-def convLayer(x, ksize, strides,out_channel, name, padding = "SAME"): 
-    """convolution"""
-    in_channel = int(x.get_shape()[-1])
-
-    conv = lambda a, b: tf.nn.conv2d(a, b, strides = [1] +strides +[ 1], padding = padding)
-
-    with tf.variable_scope(name) as scope:
-        w = tf.get_variable("w", shape = ksize+[in_channel,out_channel])
-        b = tf.get_variable("b", shape = [out_channel])
-        out_put = conv(x,w)
-        # print mergeFeatureMap.shape
-        out = tf.nn.bias_add(out_put, b)
-        return tf.nn.relu(out, name = scope.name)
 
 def myconvLayer(x, ksize, strides,out_channel, name, padding = "SAME"): 
     """convolution"""
@@ -95,20 +72,14 @@ class alexNet(object):
         with tf.variable_scope('model_%d'%self.seed):
             conv1 = complex_convLayer(self.X_com, [5, 5], [1, 1], 128, "conv1", "SAME")
             pool1 = complex_maxPoolLayer(conv1,[3, 3],[ 1,1], "pool1", "SAME")
-            norm_pool1_R=tf.layers.batch_normalization(pool1[0],training=self.training)
-            norm_pool1_I=tf.layers.batch_normalization(pool1[1],training=self.training)
 
-            conv2 = complex_convLayer([norm_pool1_R,norm_pool1_I], [3, 3], [1, 1], 64, "conv2",'SAME')
+            conv2 = complex_convLayer(pool1, [3, 3], [1, 1], 64, "conv2",'SAME')
             pool2 = complex_maxPoolLayer(conv2,[3, 3], [1, 1], "pool2", "SAME")
-            norm_pool2_R=tf.layers.batch_normalization(pool2[0],training=self.training)
-            norm_pool2_I=tf.layers.batch_normalization(pool2[1],training=self.training)
 
-            conv3 = complex_convLayer([norm_pool2_R,norm_pool2_I], [5, 5], [1, 1], 64, "conv3",'VALID')
+            conv3 = complex_convLayer(pool2, [5, 5], [1, 1], 64, "conv3",'VALID')
             pool3 = complex_maxPoolLayer(conv3, [3, 3], [2, 2], "pool3", "VALID")
-            norm_pool3_R=tf.layers.batch_normalization(pool3[0],training=self.training)
-            norm_pool3_I=tf.layers.batch_normalization(pool3[1],training=self.training)
 
-            conv4 = complex_convLayer([norm_pool3_R,norm_pool3_I], [3, 3], [1, 1], 64, "conv4",'VALID')
+            conv4 = complex_convLayer(pool3, [3, 3], [1, 1], 64, "conv4",'VALID')
             pool4 = complex_maxPoolLayer(conv4, [3, 3], [2, 2], "pool4", "VALID")
 
 
@@ -123,28 +94,14 @@ class alexNet(object):
 
     def real_fc(self,R,I,dim):
         reshape = tf.sqrt(tf.square(R)+tf.square(I))
-        norm_reshape=tf.layers.batch_normalization(reshape,training=self.training)
-        fc1 = fcLayer(norm_reshape, dim, 512, reluFlag=True, name = "fc4")
-        self.out1 = fc1[0]
-        norm_fc1=tf.layers.batch_normalization(fc1,training=self.training)
-        fc2 = fcLayer(norm_fc1, 512, 128, reluFlag=True,name =  "fc5")
-        self.out2 = fc2[0]
-        norm_fc2=tf.layers.batch_normalization(fc2,training=self.training)
-        self.fc3 = fcLayer(norm_fc2, 128, self.CLASSNUM, reluFlag=True,name =  "fc6")
+        fc1 = fcLayer(reshape, dim, 512, reluFlag=True, name = "fc4")
+        fc2 = fcLayer(fc1, 512, 128, reluFlag=True,name =  "fc5")
+        self.fc3 = fcLayer(fc2, 128, self.CLASSNUM, reluFlag=True,name =  "fc6",norm=False)
             
     def complex_fc(self,R,I,dim):
-        R=tf.layers.batch_normalization(R,training=self.training)
-        I=tf.layers.batch_normalization(I,training=self.training)
         fc1 = complex_fcLayer([R,I], dim, 512, reluFlag=True, name = "fc4")
-        self.out1 = fc1[0]
-        R=tf.layers.batch_normalization(fc1[0],training=self.training)
-        I=tf.layers.batch_normalization(fc1[1],training=self.training)
-        fc2 = complex_fcLayer([R,I], 512, 128, reluFlag=True,name =  "fc5")
-        self.out2 = fc2[0]
-        R=tf.layers.batch_normalization(fc2[0],training=self.training)
-        I=tf.layers.batch_normalization(fc2[1],training=self.training)
-        fc3 = complex_fcLayer([R,I], 128, self.CLASSNUM, reluFlag=True,name =  "fc6")
-        self.out3 = fc3[0]
+        fc2 = complex_fcLayer(fc1, 512, 128, reluFlag=True,name =  "fc5")
+        fc3 = complex_fcLayer(fc2, 128, self.CLASSNUM, reluFlag=True,name =  "fc6",norm=False)
         self.fc3 = tf.sqrt(tf.square(fc3[0])+tf.square(fc3[1]))
 
     def Learnable_relu(self,C,name):
@@ -158,7 +115,7 @@ class alexNet(object):
 
 
 
-    def complex_fcLayer(self,x, input_size, output_size, reluFlag, name):
+    def complex_fcLayer(self,x, input_size, output_size, reluFlag, name,norm=True):
         """fully-connect"""
         with tf.variable_scope(name) as scope:
             wr = tf.get_variable("wr", shape = [input_size, output_size], dtype = tf.float32)
@@ -167,6 +124,9 @@ class alexNet(object):
             bi = tf.get_variable("bi", [output_size], dtype = tf.float32)
             R = tf.nn.xw_plus_b(x[0], wr, br)- tf.nn.xw_plus_b(x[1], wi, bi)
             I = tf.nn.xw_plus_b(x[0], wi, bi)+ tf.nn.xw_plus_b(x[1], wr, br)
+            if norm :
+                R=tf.layers.batch_normalization(R,training=self.training)
+                I=tf.layers.batch_normalization(I,training=self.training)
             if reluFlag:
             #     f = tf.complex(R,I)
             #     f = tf.tanh(f)
@@ -191,6 +151,8 @@ class alexNet(object):
 
             R = conv(x[0],wr)-conv(x[1],wi) +br
             I= conv(x[1],wr)+conv(x[0],wi) +bi
+            R=tf.layers.batch_normalization(R,training=self.training)
+            I=tf.layers.batch_normalization(I,training=self.training)
             if act_flag:
                 Z = tf.complex(R,I)
                 Z=1/Z
@@ -208,7 +170,33 @@ class alexNet(object):
                               strides = [1] +strides+[1], padding = padding, name = name)]
 
 
+    def fcLayer(self,x, input_size, output_size, reluFlag, name,norm=True):
+    """fully-connect"""
+    with tf.variable_scope(name) as scope:
+        w = tf.get_variable("w", shape = [input_size, output_size], dtype = "float")
+        b = tf.get_variable("b", [output_size], dtype = "float")
+        out = tf.nn.xw_plus_b(x, w, b, name = scope.name)
+        if norm:
+            out=tf.layers.batch_normalization(out,training=self.training)
+        if reluFlag:
+            return tf.nn.relu(out)
+        else:
+            return out
 
+    def convLayer(self,x, ksize, strides,out_channel, name, padding = "SAME"): 
+        """convolution"""
+        in_channel = int(x.get_shape()[-1])
+
+        conv = lambda a, b: tf.nn.conv2d(a, b, strides = [1] +strides +[ 1], padding = padding)
+
+        with tf.variable_scope(name) as scope:
+            w = tf.get_variable("w", shape = ksize+[in_channel,out_channel])
+            b = tf.get_variable("b", shape = [out_channel])
+            out_put = conv(x,w)
+            # print mergeFeatureMap.shape
+            out = tf.nn.bias_add(out_put, b)
+            out = tf.layers.batch_normalization(out,training=self.training)
+            return tf.nn.relu(out, name = scope.name)
 
 
 
