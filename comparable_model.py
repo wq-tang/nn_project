0978c5188a58b831d8eabddf98bf92a1c8847fe8
wq-tang <291435820@qq.com>
2019-01-26 13:47:27 +0800

    all

---
+++ comparable_model.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+import numpy as np
+from model import alexNet
+
+
+def sign(x):
+    e = 0.1**8
+    return tf.nn.relu(x)/(tf.nn.relu(x)+e)
+
+
+
+class complex_net(alexNet):
+	"""docstring for complex_net"""
+    def __init__(self, x, classNum, seed,modelPath = "complexnet"):
+    	super(complex_net,self).__init__(x, classNum, seed,modelPath)
+    	tf.set_random_seed(seed) 
+        self.buildCNN()
+
+
+    def build_complex_CNN_for_mnist(self):
+    	with tf.variable_scope('model_%d'%self.seed):
+            conv1 = self.complex_convLayer(self.X_com, [5, 5], [1, 1], 16, "conv1", "SAME")
+            pool1 = self.complex_maxPoolLayer(conv1,[2, 2],[ 1,1], "pool1", "SAME")
+
+            conv2 = self.complex_convLayer(pool1, [3, 3], [1, 1], 64, "conv2",'SAME')
+            pool2 = self.complex_maxPoolLayer(conv2,[2, 2], [1, 1], "pool2", "SAME")
+
+            # conv3 = self.complex_convLayer(pool2, [5, 5], [1, 1], 64, "conv3",'VALID')
+            # pool3 = self.complex_maxPoolLayer(conv3, [3, 3], [2, 2], "pool3", "VALID")
+            cnnout = pool2
+            shapes = cnnout[0].get_shape().as_list()[1:]
+            mul = reduce(lambda x,y:x * y,shapes)
+            R = tf.reshape(cnnout[0],[-1,mul])
+            I = tf.reshape(cnnout[1],[-1,mul])
+            dim = R.get_shape()[1].value
+
+            fc1 = self.complex_fcLayer([R,I], dim, 512, reluFlag=True, name = "fc4")
+        	fc2 = self.complex_fcLayer(fc1, 512, self.CLASSNUM, reluFlag=False,name =  "fc5")
+        	self.out = self.fc2
+        	self.out = tf.sqrt(tf.square(out[0])+tf.square(out[1]))
+
+    def buildCNN_real_CNN_for_mnist(self):
+      	with tf.variable_scope('model_%d'%self.seed):
+            conv1 = self.convLayer(self.X, [5, 5], [1, 1], 16, "conv1", "SAME")
+            pool1 = self.maxPoolLayer(conv1,[2, 2],[ 1,1], "pool1", "SAME")
+
+            conv2 = self.convLayer(pool1, [3, 3], [1, 1], 64, "conv2",'SAME')
+            pool2 = self.maxPoolLayer(conv2,[2, 2], [1, 1], "pool2", "SAME")
+
+            # conv3 = self.complex_convLayer(pool2, [5, 5], [1, 1], 64, "conv3",'VALID')
+            # pool3 = self.complex_maxPoolLayer(conv3, [3, 3], [2, 2], "pool3", "VALID")
+            cnnout = pool2
+            shapes = cnnout.get_shape().as_list()[1:]
+            mul = reduce(lambda x,y:x * y,shapes)
+            Res = tf.reshape(cnnout,[-1,mul])
+            dim = R.get_shape()[1].value
+            fc1 = self.fcLayer(Res, dim, 512, reluFlag=True, name = "fc4")
+        	fc2 = self.fcLayer(fc1, 512, self.CLASSNUM, reluFlag=False,name =  "fc5")
+        	self.out = self.fc2
+        	self.out = tf.sqrt(tf.square(out[0])+tf.square(out[1]))
+
+
+
+    def build_complex_CNN_for_cifar10(self):
+        with tf.variable_scope('model_%d'%self.seed):
+            conv1 = self.complex_convLayer(self.X_com, [5, 5], [1, 1], 64, "conv1", "SAME")
+            pool1 = self.complex_maxPoolLayer(conv1,[2, 2],[ 1,1], "pool1", "SAME")
+
+            conv2 = self.complex_convLayer(pool1, [3, 3], [1, 1], 64, "conv2",'SAME')
+            pool2 = self.complex_maxPoolLayer(conv2,[2, 2], [1, 1], "pool2", "SAME")
+
+            # conv3 = self.complex_convLayer(pool2, [5, 5], [1, 1], 64, "conv3",'VALID')
+            # pool3 = self.complex_maxPoolLayer(conv3, [3, 3], [2, 2], "pool3", "VALID")
+            cnnout = pool2
+            shapes = cnnout[0].get_shape().as_list()[1:]
+            mul = reduce(lambda x,y:x * y,shapes)
+            R = tf.reshape(cnnout[0],[-1,mul])
+            I = tf.reshape(cnnout[1],[-1,mul])
+            dim = R.get_shape()[1].value
+
+            fc1 = self.complex_fcLayer([R,I], dim, 384, reluFlag=True, name = "fc4")
+            fc2 = self.complex_fcLayer(fc1, 384, 192, reluFlag=False,name =  "fc5")
+            fc3 = self.complex_fcLayer(fc2, 192, self.CLASSNUM, reluFlag=True,name =  "fc6",norm=False)
+            self.out = fc3
+            self.out = tf.sqrt(tf.square(out[0])+tf.square(out[1]))
+
+
+    def buildCNN_real_CNN_for_cifar10(self):
+        with tf.variable_scope('model_%d'%self.seed):
+            conv1 = self.convLayer(self.X, [5, 5], [1, 1], 64, "conv1", "SAME")
+            pool1 = self.maxPoolLayer(conv1,[2, 2],[ 1,1], "pool1", "SAME")
+
+            conv2 = self.convLayer(pool1, [3, 3], [1, 1], 64, "conv2",'SAME')
+            pool2 = self.maxPoolLayer(conv2,[2, 2], [1, 1], "pool2", "SAME")
+
+            # conv3 = self.complex_convLayer(pool2, [5, 5], [1, 1], 64, "conv3",'VALID')
+            # pool3 = self.complex_maxPoolLayer(conv3, [3, 3], [2, 2], "pool3", "VALID")
+            cnnout = pool2
+            shapes = cnnout.get_shape().as_list()[1:]
+            mul = reduce(lambda x,y:x * y,shapes)
+            Res = tf.reshape(cnnout,[-1,mul])
+            dim = R.get_shape()[1].value
+            fc1 = self.fcLayer(Res, dim, 384, reluFlag=True, name = "fc4")
+            fc2 = self.fcLayer(fc1, 384, 192, reluFlag=False,name =  "fc5")
+            fc3 = self.complex_fcLayer(fc2, 192, self.CLASSNUM, reluFlag=True,name =  "fc6",norm=False)
+            self.out = fc3
+            self.out = tf.sqrt(tf.square(out[0])+tf.square(out[1]))
---
+++ compare.py
@@ -0,0 +1,100 @@
+import os
+import numpy as np 
+import tensorflow as tf 
+import time
+import cifar10_input
+import math
+from comparable_model import complex_net
+##cifar batch =128  epoch = 50000
+##mnist epoch=50  bathch = 60000
+
+
+model_path =os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'romania_complex.ckpt') 
+def main():
+	def loss(logits,y):
+		labels =tf.cast(y,tf.int64)
+		cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels = y,name='cross_entropy_per_example')
+		cross_entropy_mean = tf.reduce_mean(cross_entropy,name='cross_entropy')
+		tf.add_to_collection('losses',cross_entropy_mean)
+		return tf.add_n(tf.get_collection('losses'),name='total_loss')
+	def test():
+		precision=[]
+		for i in range(40):
+			test_x,test_y = sess.run([test_images,test_labels])
+			precision.append(accuracy.eval(feed_dict={x:test_x, y: test_y}))
+		return np.mean(precision)
+
+	max_epoch = 50000
+	batch_step = 128
+
+	data_dir =os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'cifar-10-batches-bin')
+	# cifar10.maybe_download_and_extract()
+	train_images ,train_labels = cifar10_input.distorted_inputs(data_dir=data_dir,batch_size = batch_step)
+	test_images,test_labels = cifar10_input.inputs(eval_data = True,data_dir=data_dir,batch_size=1000)
+	x  = tf.placeholder(tf.float32,[None,24,24,3])
+	y = tf.placeholder(tf.int32,[None])
+
+	model = complex_net
+	models_result =model.out
+	loss  = loss(models_result,y)
+
+	update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+	with tf.control_dependencies(update_ops):
+		train_op = tf.train.AdamOptimizer(0.1**3).minimize(loss)
+	top_k_op = tf.nn.in_top_k(models_result,y,1)
+	accuracy = tf.reduce_mean(tf.cast(top_k_op,tf.float32))
+	sess = tf.InteractiveSession()
+	tf.global_variables_initializer().run()
+	tf.train.start_queue_runners()
+
+	train_list = []
+	test_list=[]
+
+	test_x,test_y = sess.run([test_images,test_labels])
+	for i in range(max_epoch):
+		start_time = time.time()
+		train_x,train_y = sess.run([train_images,train_labels])
+		_,loss_value = sess.run([train_op,loss],feed_dict={x:train_x,y:train_y})
+		duration = time.time() - start_time
+		if i%100 ==0:
+			examples_per_sec = batch_step/duration
+			sec_per_batch = float(duration)
+			format_str = ('step %d,loss=%.2f (%.1f examples/sec; %.3f sec/batch)')
+			print(format_str %(i,loss_value,examples_per_sec,sec_per_batch))
+
+			train_accuracy = accuracy.eval(feed_dict={x:train_x, y:train_y})
+			for m in model:
+				m.training = False
+			# pre = test()
+			y_hat = models_result[:10].eval(feed_dict={x:test_x, y: test_y})
+			test_accuracy = accuracy.eval(feed_dict={x:test_x, y: test_y})
+			for m in model:
+				m.training = True
+			print( "step %d, training accuracy %g"%(i, train_accuracy))
+			print( "step %d,test accuracy %g"%(i,test_accuracy))
+			# print('pre:%g'%pre)
+			# print(y_hat)
+			train_list.append(train_accuracy)
+			test_list.append(test_accuracy)
+			# if test_accuracy>0.95:
+			# 	break
+		if i>20000:
+			with tf.control_dependencies(update_ops):
+				train_op = tf.train.AdamOptimizer(0.1**4).minimize(loss)
+
+	saver = tf.train.Saver()
+	save_path = saver.save(sess,model_path)
+	
+	for m in model:
+		m.training = False
+	pre= test()
+	for m in model:
+		m.training = True
+	print('precision @1 = %.3f'%pre)
+
+
+
+
+if __name__=='__main__':
+	main()
+
