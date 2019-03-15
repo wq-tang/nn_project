e6967aa46cc2fe8d6e13aef0db43372f38c58689
wqtang <291435820@qq.com>
2019-03-15 08:20:34 +0800

    merge

--- bagging.py
+++ bagging.py
@@ -38,8 +38,8 @@ def cifar10(path,local_path,is_complex,model_num,is_training=True):
 			precision.append(accuracy.eval(feed_dict={x:test_x, y: test_y}))
 		return np.mean(precision)
 
-	model_path =os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'/'+local_path +'/cifar10.ckpt')
-	max_epoch = 50000
+	model_path =os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),local_path)
+	max_epoch = 500
 	batch_step = 128
 	log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'cifar10_board/'+path)
 	data_dir =os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'cifar-10-batches-bin')
@@ -120,7 +120,7 @@ def cifar10(path,local_path,is_complex,model_num,is_training=True):
 		return None
 	else:
 		model_file=tf.train.latest_checkpoint('ckpt/')
-		image,lable=cifar10_input.inputs(eval_data = True,data_dir=data_dir,batch_size=10000)
+		image,lable=cifar10_input.inputs(eval_data = True,data_dir=data_dir,batch_size=1000)
 		x_,y_ = sess.run([image,lable])
 
 		saver.restore(sess,model_path)
@@ -139,13 +139,16 @@ def cifar10(path,local_path,is_complex,model_num,is_training=True):
 
 
 if __name__=='__main__':
-	path = sys.argv[1]
-	is_complex = bool(int(sys.argv[3]))
-	model_num = int(sys.argv[4])
-	local_path=sys.argv[2]
-	is_training = bool(int(sys.argv[5]))
-	print(('board_path:%s\nmodel_path:%s\nmodel_num:%d ')%(path,local_path,model_num))
-	print("is_complex:",is_complex)
-	print("is_training:",is_training)
-	cifar10(path=path,local_path=local_path ,is_complex=is_complex,model_num=model_num,is_training = is_training)
+	# path = sys.argv[1]
+	# is_complex = bool(int(sys.argv[3]))
+	# model_num = int(sys.argv[4])
+	# local_path=sys.argv[2]
+	# is_training = bool(int(sys.argv[5]))
+	# print(('board_path:%s\nmodel_path:%s\nmodel_num:%d ')%(path,local_path,model_num))
+	# print("is_complex:",is_complex)
+	# print("is_training:",is_training)
+	# cifar10(path=path,local_path=local_path ,is_complex=is_complex,model_num=model_num,is_training = is_training)
+	res1=cifar10(path='rm_test/cifar10_1.ckpt',local_path='rm_test' ,is_complex=False,model_num=1,is_training = False)
+	res2=cifar10(path='rm_test/cifar10_2.ckpt',local_path='rm_test' ,is_complex=False,model_num=1,is_training = False)
+
 
