77e1a97b92e673bc4697a0f90c2ba912f60384ef
wq-tang <291435820@qq.com>
2019-03-22 15:24:38 +0800

    second

--- second_st.py
+++ second_st.py
@@ -38,7 +38,7 @@ def complex_model(x,class_num):
 	I = out[:,1,:]
 	return tf.sqrt(tf.square(R)+tf.square(I))
 
-def Secondary_net(model_name,is_complex):
+def Secondary_net(model_name,combine_list,is_complex):
 	#修改文件读取名字，读取函数
 	### Using the class ###
 	def loss(logits,y):
@@ -47,51 +47,60 @@ def Secondary_net(model_name,is_complex):
 		cross_entropy_mean = tf.reduce_mean(cross_entropy,name='cross_entropy')
 		tf.add_to_collection('losses',cross_entropy_mean)
 		return tf.add_n(tf.get_collection('losses'),name='total_loss')
-	def test(test_batch):
+	def test(test_batch,test_feed_dicts):
 		precision=[]
 		for i in range(10):
 			test_x1,_ = next(test_data[0])
 			test_x2,_ = next(test_data[1])
 			test_x3,_ = next(test_data[2])
 			test_x4,test_y = next(test_data[3])
-			precision.append(accuracy.eval(feed_dict={x1:test_x1,x2:test_x2,x3:test_x3,x4:test_x4, y: test_y}))
+			precision.append(accuracy.eval(feed_dict=test_feed_dicts))
 		return np.mean(precision)
 
 	#修改读取文件的函数以及文件名称
 	#修改输出路径
 	if is_complex:
-		tail = model_name+'_complex_board'
+		board_tail = model_name+'_complex_board'+''.join(combine_list)
 	else:
-		tail = model_name+'_real_board'
-	log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'mynet/stacking/'+tail)
+		board_tail = model_name+'_real_board'+''.join(combine_list)
+
+	log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'mynet/stacking/'+board_tail)
 	if is_complex:
 		tail = 'complex'
 	else:
 		tail = 'real'
-	max_epoch = 30000  #修改
+	max_epoch = 10000  #修改
 	batch_step = 128
 	class_num = 10   ###修改
 	train_data = []
 	test_data = []
-	for i in range(4):
-		batchTrain, batchTest = read_data(model_name+'_'+tail+str(i+1)+'.h5',batch_step,1000)
+	for i in range(len(combine_list)):
+		batchTrain, batchTest = read_data(model_name+'_'+tail+combine_list[i]+'.h5',batch_step,1000)
 		train_data.append(batchTrain)
 		test_data.append(batchTest)
 
 	y = tf.placeholder(tf.int32,[None])
-
+	names = locals()
 	if is_complex:
-		x1 = tf.placeholder(tf.float32,[None,2,class_num],name = 'input_x1')
-		x2 = tf.placeholder(tf.float32,[None,2,class_num],name = 'input_x2')
-		x3 = tf.placeholder(tf.float32,[None,2,class_num],name = 'input_x3')
-		x4 = tf.placeholder(tf.float32,[None,2,class_num],name = 'input_x4')
-		out = complex_model([x1,x2,x3,x4],class_num)
+		for i in range(len(combine_list)):
+			names['x' + str(i+1) ] = tf.placeholder(tf.float32,[None,2,class_num],name = 'input_x'+str(i+1))
+		if len(combine_list)==4:
+			x = [x1,x2,x3,x4]
+		elif len(combine_list)==3:
+			x=[x1,x2,x3]
+		else:
+			x=[x1,x2]
+		out = complex_model(x,class_num)
 	else:
-		x1 = tf.placeholder(tf.float32,[None,class_num],name = 'input_x1')
-		x2 = tf.placeholder(tf.float32,[None,class_num],name = 'input_x2')
-		x3 = tf.placeholder(tf.float32,[None,class_num],name = 'input_x3')
-		x4 = tf.placeholder(tf.float32,[None,class_num],name = 'input_x4')
-		out = real_model([x1,x2,x3,x4],class_num)
+		for i in range(len(combine_list)):
+			names['x' + str(i+1) ] = tf.placeholder(tf.float32,[None,class_num],name = 'input_x'+str(i+1))
+		if len(combine_list)==4:
+			x = [x1,x2,x3,x4]
+		elif len(combine_list)==3:
+			x=[x1,x2,x3]
+		else:
+			x=[x1,x2]
+		out = real_model(x,class_num)
 
 
 	with tf.name_scope('loss'):
@@ -115,30 +124,41 @@ def Secondary_net(model_name,is_complex):
 	tf.train.start_queue_runners()
 
 	ans = []
-	test_x1,_ = next(test_data[0])
-	test_x2,_ = next(test_data[1])
-	test_x3,_ = next(test_data[2])
-	test_x4,test_y = next(test_data[3])
+	tests = locals()
+	for i in range(len(combine_list)-1):
+		tests['test_x' + str(i+1) ] ,_ = next(test_data[i])
+	tests['test_x' + str(len(combine_list)+1) ],test_y = next(test_data[len(combine_list)])
+
+	if len(combine_list)==4:
+		train_feed_dicts={x1:train_x1,x2:train_x2,x3:train_x3,x4:train_x4,y:train_y}
+		test_feed_dicts = {x1:test_x1,x2:test_x2,x3:test_x3,x4:test_x4,y:test_y}
+	elif len(combine_list)==3:
+		train_feed_dicts={x1:train_x1,x2:train_x2,x3:train_x3,y:train_y}
+		test_feed_dicts = {x1:test_x1,x2:test_x2,x3:test_x3,y:test_y}
+	else:
+		train_feed_dicts={x1:train_x1,x2:train_x2,y:train_y}
+		test_feed_dicts = {x1:test_x1,x2:test_x2,y:test_y}
 
 	for i in range(max_epoch):
 		start_time = time.time()
-		train_x1,_ = next(train_data[0])
-		train_x2,_ = next(train_data[1])
-		train_x3,_ = next(train_data[2])
-		train_x4,train_y = next(train_data[3])
-		_ = sess.run( train_op, feed_dict={x1:train_x1,x2:train_x2,x3:train_x3,x4:train_x4,y:train_y})
+		trains = locals()
+		for i in range(len(combine_list)-1):
+			trains['train_x' + str(i+1) ] ,_ = next(train_data[i])
+		trains['train_x' + str(len(combine_list)+1) ],test_y = next(train_data[len(combine_list)])
+		
+		_ = sess.run( train_op, feed_dict=train_feed_dicts)
 		duration = time.time() - start_time
 		if i%400 ==0:
 			examples_per_sec = batch_step/duration
 			sec_per_batch = float(duration)
 			format_str = ('step %d,loss=%.2f (%.1f examples/sec; %.3f sec/batch_step)')
-			summary,loss_value = sess.run([merged,loss], feed_dict={x1:train_x1,x2:train_x2,x3:train_x3,x4:train_x4,y:train_y})
+			summary,loss_value = sess.run([merged,loss], feed_dict=train_feed_dicts)
 			train_writer.add_summary(summary, i)
-			train_accuracy = accuracy.eval(feed_dict={x1:train_x1,x2:train_x2,x3:train_x3,x4:train_x4, y:train_y})
+			train_accuracy = accuracy.eval(feed_dict=train_feed_dicts)
 			print(format_str %(i,loss_value,examples_per_sec,sec_per_batch))
-			summary, acc = sess.run([merged, accuracy], feed_dict={x1:test_x1,x2:test_x2,x3:test_x3,x4:test_x4,y:test_y})
+			summary, acc = sess.run([merged, accuracy], feed_dict=test_feed_dicts)
 			test_writer.add_summary(summary, i)
-			test_accuracy = test(test_data)
+			test_accuracy = test(test_data,test_feed_dicts)
 			ans.append(test_accuracy)
 			print( "step %d, training accuracy %g"%(i, train_accuracy))
 			print( "step %d,test accuracy %g"%(i,test_accuracy))
@@ -147,4 +167,5 @@ def Secondary_net(model_name,is_complex):
 	print('precision @1 = %.5f'%np.mean(ans[-10:]))
 
 if __name__=='__main__':
-	Secondary_net('MNIST',is_complex=False)
+	combine_list = ['1','2','3','4']
+	Secondary_net('MNIST',combine_list,is_complex=False)
